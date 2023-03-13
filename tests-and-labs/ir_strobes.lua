3b7d2919ced01e5b1c1c604ff03740c8128046e2
Johan <johan.stromhielm@gmail.com>
2023-03-13 22:59:10 +0100

    correct table index

--- tests-and-labs/ir_strobes.lua
+++ tests-and-labs/ir_strobes.lua
@@ -1,10 +1,6 @@
 strobing = {}
 strobes = {}
 
-function startsWith(str, prefix)
-	return str:sub(1, #prefix) == prefix
-end
-
 function destroyStrobes()
     for i,strobe in pairs(strobes) do
         strobe:destroy()
@@ -15,28 +11,34 @@ end
 function createStrobes()
     destroyStrobes()
     if #strobing < 1 then
-        return        
+        trigger.action.outText('STROBING IS EMPTY!!! ', 10)    
+        return
     end
-    for i,j in pairs(strobing) do
-        strobes[i] = Spot.createInfraRed(j, {x=0,y=-1,z=0}, j:getPoint())
+    for i,unit in pairs(strobing) do
+        local id = #strobes + 1
+        --trigger.action.outText('Creating strobe ' .. id .. ' on unit: ' .. unit:getName(), 10)
+        strobes[id] = Spot.createInfraRed(unit, {x=0,y=1,z=0}, unit:getPoint())
     end
-    timer.scheduleFunction(destroyStrobes, {}, timer.getTime() + 0.2)
+    timer.scheduleFunction(destroyStrobes, {}, timer.getTime() + 0.25)
     timer.scheduleFunction(createStrobes, {}, timer.getTime() + 1)    
 end
 
-function scanForGroundStrobers(side)
+function scanForGroundStrobers(side, prefix)
+    strobing = {}
     local blueStrobes = coalition.getGroups(side, Group.Category.GROUND)
     for i,j in pairs(blueStrobes) do
-        if startsWith(j:getName(), "STROBE ") then
-            for k,l in pairs(j:getUnits()) do
-                strobing[l:getID()] = l
+        if j:getName():sub(1, #prefix) == prefix then
+            for index,unit in pairs(j:getUnits()) do                
+                local id = #strobing+1
+                strobing[id] = unit
+                local a = strobing[id]
             end
         end    
     end
 end
 
 function allStrobesOn()
-    scanForGroundStrobers(coalition.side.BLUE)
+    scanForGroundStrobers(coalition.side.BLUE, "STROBE ")
     timer.scheduleFunction(createStrobes, {}, timer.getTime() + 1)    
 end
 
