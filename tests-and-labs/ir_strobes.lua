2f93763c8ed519f5d4ef46aca4db58170f97df2b
Johan <johan.stromhielm@gmail.com>
2024-03-01 21:15:28 +0100

    strobes work now

--- tests-and-labs/ir_strobes.lua
+++ tests-and-labs/ir_strobes.lua
@@ -10,16 +10,12 @@ end
 
 function createStrobes()
     destroyStrobes()
-    if #strobing < 1 then
-        trigger.action.outText('STROBING IS EMPTY!!! ', 10)    
-        return
-    end
     for i,unit in pairs(strobing) do
         local id = #strobes + 1
-        --trigger.action.outText('Creating strobe ' .. id .. ' on unit: ' .. unit:getName(), 10)
+        --env.info('Creating strobe ' .. id .. ' on unit: ' .. unit:getName())
         strobes[id] = Spot.createInfraRed(unit, {x=0,y=1,z=0}, unit:getPoint())
     end
-    timer.scheduleFunction(destroyStrobes, {}, timer.getTime() + 0.25)
+    timer.scheduleFunction(destroyStrobes, {}, timer.getTime() + 0.1)
     timer.scheduleFunction(createStrobes, {}, timer.getTime() + 1)    
 end
 
@@ -29,7 +25,7 @@ function scanForGroundStrobers(side, prefix)
     for i,j in pairs(blueStrobes) do
         if j:getName():sub(1, #prefix) == prefix then
             for index,unit in pairs(j:getUnits()) do                
-                local id = #strobing+1
+                local id = #strobing + 1
                 strobing[id] = unit
                 local a = strobing[id]
             end
@@ -39,6 +35,10 @@ end
 
 function allStrobesOn()
     scanForGroundStrobers(coalition.side.BLUE, "STROBE ")
+    if #strobing < 1 then
+        trigger.action.outText('STROBING IS EMPTY!!! ', 10)    
+        return
+    end
     timer.scheduleFunction(createStrobes, {}, timer.getTime() + 1)    
 end
 
